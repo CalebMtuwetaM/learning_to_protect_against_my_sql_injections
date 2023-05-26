4db1fc63e086295abcf849e07daca4d5cbe3e9ad
CalebMtuwetaM <73331147+CalebMtuwetaM@users.noreply.github.com>
2023-05-26 17:43:38 +0000

    added the database and the login interface

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+# import the nessecary module
 import sqlite3
 
+# create the database table
 conn = sqlite3.connect("people.db")
+
+# setup the data in the database
 
 cursor = conn.cursor()
 
@@ -11,7 +15,7 @@ cursor.execute("""CREATE TABLE people (
   job VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   age INT,
-  gender CHAR(1))""")
+  gender CHAR(1));""")
 
 cursor.execute("""INSERT INTO people (name,job,password,age,gender) VALUES
 ('Mike','Programmer','mypass123','45','m'),
@@ -20,3 +24,21 @@ cursor.execute("""INSERT INTO people (name,job,password,age,gender) VALUES
 ('Sam','Accountant','caleb5608','65','m');""")
 
 conn.commit()
+
+# a sort of a login interface where you input your name and your password and retrieve information about yourself
+
+name_input = input("What is your name: ")
+pass_input = input("What is your password: ")
+
+
+cursor.execute(f"SELECT * FROM people WHERE name = '{name_input}' AND password = '{pass_input}'")
+
+rows = cursor.fetchall()
+
+
+if len(rows) == 0:
+    print("Login Failed!")
+else:
+    print(f"Success! Here is the information of {name_input}")
+    for row in rows:
+        print(row)
